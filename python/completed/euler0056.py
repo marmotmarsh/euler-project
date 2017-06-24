7fac9230e792d524844abac3f25a86379e56dcd4
Holden <marmotmarsh74@gmail.com>
2017-06-24 11:44:15 -0700

    Finished 56

---
+++ python/completed/euler0056.py
@@ -0,0 +1,24 @@
+###
+#
+# Created by Holden on 6/24/2016
+#
+# COMPLETED on 6/24/2017
+#
+# Problem 56 - Powerful Digit Sum
+#
+###
+
+
+def powerful_digit_sum(bound):
+    max_sum = 0
+    for a in range(1, bound+1):
+        for b in range(1, bound+1):
+            power = a**b
+            cur_sum = sum(list(map(int, list(str(power)))))
+            #print("a = {}, b = {}, power = {}, sum = {}".format(a, b, power, cur_sum))
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+    
+    return max_sum
+
+print(powerful_digit_sum(100))
---
+++ python/euler0057.py
@@ -0,0 +1,16 @@
+###
+#
+# Created by Holden on 6/24/2016
+#
+#
+#
+# Problem 57 - Square Root Convergents
+#
+###
+
+
+def square_root_convergents():
+
+    return 0
+
+print(square_root_convergents())
