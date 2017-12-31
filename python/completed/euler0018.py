f987f0931ddfb78f6927043b99e2b57a329a5109
Holden Marsh <holden@creditiq.com>
2017-12-31 12:59:34 -0800

    pylint

--- python/completed/euler0018.py
+++ python/completed/euler0018.py
@@ -10,20 +10,20 @@
 
 pyramid = open("../data/x0018.txt", "r")
 pyramid_list = []
-    
+
 for line in pyramid:
     pyramid_list.append(list(map(int, line.strip().split(' '))))
 
 
 def max_path_sum_i():
-    current_row = pyramid_list[len(pyramid_list)-1]
+    current_row = pyramid_list[len(pyramid_list) - 1]
 
-    for i in range(len(pyramid_list)-2, -1, -1):
+    for i in range(len(pyramid_list) - 2, -1, -1):
         lower_row = current_row
         current_row = pyramid_list[i]
 
-        for j in range(i+1):
-            current_row[j] += max((lower_row[j], lower_row[j+1]))
+        for j in range(i + 1):
+            current_row[j] += max((lower_row[j], lower_row[j + 1]))
 
     return current_row[0]
 
