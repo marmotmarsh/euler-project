8ea3939161a0b2e5895828021bb325e4216aafb1
marmotmarsh <metalmarmot75@gmail.com>
2021-04-25 09:32:14 -0700

    JS #9

--- javascript/completed/euler0009.js
+++ javascript/completed/euler0009.js
@@ -1,1 +1,14 @@
-export async function solve9() {}
+export async function solve9() {
+  var n = 1000;
+  var max = -1;
+  for (var i = 3; i < n / 3; i++) {
+    for (var j = i + 1; j < n - (i + j); j++) {
+      var k = n - (i + j);
+      if (Math.sqrt(i * i + j * j) === k) {
+        var product = i * j * k;
+        max = product > max ? product : max;
+      }
+    }
+  }
+  return max;
+}
