4dd9c557ab61151dfad478dd632d94d0567599f3
marmotmarsh <metalmarmot75@gmail.com>
2021-06-23 09:10:38 -0700

    Make main function better

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,14 @@
-import ptimeout from 'promise.timeout';
-
 import { solve } from './solve.js';
 
 async function main() {
   const n = process.argv[2];
-  solve(n);
+  const timerString = `Solving problem ${n}`;
+
+  console.time(timerString);
+
+  await solve(n);
+
+  console.timeEnd(timerString);
 }
 
-const solution = ptimeout(main, 60000);
-solution();
+main();
